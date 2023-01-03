774593fc1690b4eb43fddc71b01ab93072da4b4f
Punya Alahakoon <78515540+PunyaAlahakoon@users.noreply.github.com>
2023-01-03 16:58:41 +1100

    Add files via upload

---
+++ parameter_estimation/Step1 (a)/Golden_Valley, ND/abc_smc_US.m
@@ -0,0 +1,154 @@
+%run this for all the counties.  
+tic
+
+%load a vetor to specify which day to start the epidemic for parameter
+%estimation 
+st_days=load("dt.mat","de");
+st_days=st_days.de;
+
+%load county population sizes:
+pops=readtable('pop_sizes_2019.csv');
+pop_N=table2array(pops);
+
+%load case data:
+ data=readtable('case_numbers.csv');
+ y=table2array(data(:,2:13));
+
+%load death data:
+dt=readtable("deaths.csv");
+ds=table2array(dt(:,2:13));
+
+
+
+ %vaccine date:
+ vim=zeros(14,12); %shift the vaccinations to 14 days 
+vt1=readtable("dose1_time_series.csv");
+vv1=table2array(vt1(:,2:13));
+v1=[vim;vv1(1:end-14,:)];
+vt2=readtable("dose2_time_series.csv");
+vv2=table2array(vt2(:,2:13));
+v2=[vim;vv2(1:end-14,:)];
+
+
+
+
+ dim=size(y,2); %number of counties to consider 
+
+%Number of particles/ parameters sets to sample using ABC-SMC
+B=1000;
+
+%number of generations to run:
+G=4;
+
+count=[11 16 17 1 2];%these are the same for all teh counties 
+%counts=[11=daily_notified cases 15:17=deaths 4=dose 1 5=dose2]
+
+%store parameters:
+np=15; %number of parameters to estimate 
+para_smc={1,dim};
+sx={1,dim};
+accepted_case_paths={1,dim};
+accepted_dose1_paths={1,dim};
+accepted_dose2_paths={1,dim};
+w_smc=zeros(B,dim); % store weights
+
+%Es=Es';
+AG_smc=zeros(G,dim);%store the # of particles generated to get B parameters(if needed) 
+s_x= zeros(B,dim);%save the distance criteria (if needed)
+
+for k=10:10
+    %input data:
+    N=pop_N(k); %initial population size 
+   % ind=find(isnan(y(st_days(k):end,k))); in=1; Don't need this anymore:
+   % start days are different for counties 
+    cases=y(st_days(k):end,k); deaths=ds(st_days(k):end,k); 
+    
+    %vaccine first dose availbility: depends on states SD:12/15/2020 ND: 12/14/2020
+   if ismember(k,1:6) %SD; k=1:6
+        vi=264-st_days(k);  %264 is the day of vaccine availbility out of original time frame 
+   else
+        vi=263-st_days(k);
+   end
+
+    %vaccines=[v1(vi:end,k) v2(vi:end,k)]; %there are NaNs at the end! check with distance metrics 
+    %vaccines=[v1(:,k) v2(:,k)];
+    %find the day to start the vaccine time-series: 
+    vcc1=(v1(st_days(k):end,k)); vcc2= (v2(st_days(k):end,k));
+    ind=[find(isnan(vcc1), 1, 'last' ) find(isnan(vcc2), 1, 'last' )]; 
+    cv=[find(vcc1(ind(1)+1:end)~=0,1,'first')+ind(1) find(vcc2(ind(2)+1:end)~=0,1,'first')+ind(2)];
+    %cv(1) and cv(2) are the same for both doses
+    
+    vaccines=[[zeros(cv(1),1); movmean(vcc1(cv(1)+1:end),7)] [zeros(cv(2),1); movmean(vcc2(cv(2)+1:end),7)]];
+    vacc_cum=[vcc1(cv(1)) vcc2(cv(2))];
+
+    times=[0 vi cv(1)+1 length(cases)];
+    %times: to when to start the dose 1, dose 2: relative to case occurance
+    
+    %load pre-defined tolerance vaues depending on the population:
+    E=load(['E_' num2str(k) '.mat']);
+    % Es=load('Es'); %load tolerance values 
+    E=E.E;
+    e=E(1,:);
+    %set a counter for number of iterations to run for each gen 
+    AG=zeros(1,G);
+
+    %store parameter sets
+    params=zeros(B,np);
+    w=zeros(1,B); %store weights 
+    %es=zeros(G+1,4);
+     case_paths=zeros(length(cases),B);
+    dose1_paths1=zeros(length(cases),B);
+    dose2_paths1=zeros(length(cases),B);
+    es(1,:)=e;
+    g=1;
+
+    while (g<1+G) %number of generation 
+    %store values for the current generation
+    params0=zeros(B,np);
+    w0=zeros(1,B); %store weights 
+    ag=0;%set the counter 
+    ag0=zeros(1,B);%set the counter 
+    rho_m=zeros(B,7);%store the distance values 
+    accepted_case_paths0=zeros(length(cases),B);
+    dose1_paths0=zeros(length(cases),B);
+    dose2_paths0=zeros(length(cases),B);
+     parfor a=1:B %particle number     
+     [params0(a,:),w0(a),rho_m(a,:),ag0(a),accepted_case_paths0(:,a),...
+         dose1_paths0(:,a),dose2_paths0(:,a)]=abc_ind_step(g,B,params,w,cases,deaths,vaccines,vacc_cum,e,count,times,N);
+     end 
+        
+       % e=E(g+1);
+        %e=median(rho_m);
+         e=E(g+1,:);
+      % es(g+1)=e;
+        %normalize the weights 
+        w0=normalize(w0,'norm',1);
+        %replecae the previous gen values from the new gen values
+        params=params0;
+        w=w0; %store weights 
+        AG(g)=sum(ag0);
+        case_paths=accepted_case_paths0;
+        dose1_paths1=dose1_paths0;
+        dose2_paths1=dose2_paths0;
+        g=g+1 %update the generation number 
+    end
+%store the final values for the counties:
+para_smc{1,k}=params;
+w_smc(:,k)=w; %weights 
+%E_smc(:,k)=es;%tolerance values 
+AG_smc(:,k)=AG;%number of steps generated to get B parameters 
+sx{1,k}= rho_m;
+accepted_case_paths{1,k}=case_paths; 
+accepted_dose1_paths{1,k}=dose1_paths1;
+accepted_dose2_paths{1,k}=dose2_paths1;
+end
+
+save('para_smc_Golden_Valley.mat','para_smc')
+save('case_paths_Golden_Valley.mat',"accepted_case_paths")
+save('dose1_paths_Golden_Valley.mat',"accepted_dose1_paths")
+save('dose2_paths_Golden_Valley.mat',"accepted_dose2_paths")
+save('w_smc_Golden_Valley.mat','w_smc')
+save('AG_smc_Golden_Valley.mat','AG_smc')
+save('sx_Golden_Valley.mat','sx')
+
+toc
---
+++ parameter_estimation/Step1 (a)/Golden_Valley, ND/distance_metrics5.m
@@ -0,0 +1,53 @@
+
+%%%THIS VERSION:: TO USE 1) TOLERANCE FOR THE EPI-CURVE 2) TOLERANCE FOR THE RE-INTRODUCTIONS 
+
+%depends on the generation 
+
+%calculate distance criteria 
+%counts=[11=daily_notified cases 16:17=deaths 4=dose 1 5=dose2]
+%vaccines are moving averages 
+
+function[dists]=distance_metrics5(daily_c,inci,cases,deaths,vaccines,vaccine_cums,times)
+        %1) cases:using euclidean distances:
+        %For Miner epi-curve is 1-159 days (AFTER shifting the time-series
+        %to ignore 
+        ox=daily_c(:,1);%generated 
+        cs=abs(ox-cases); %observed
+        dists(1)=sqrt(sum((cs.^2)));
+     
+        %2) deaths: absolute difference
+        dx=sum(inci(end,2:3));
+        dists(2)=abs(dx-deaths(end));
+
+        %3) vaccines: DOSE 1:euclidean distance of cumulative numbers 
+        %vx1=daily_c(times(2)+1:end,4);
+        t=times(3):length(cases);
+        vx1=daily_c(t,4);
+        vm1=movmean(vx1,7);
+        %take moving average:
+        %vm1=movmean(vaccines(t,1),7);
+    
+        v1=abs(vm1-vaccines(t,1));
+        dists(3)=sqrt(sum((v1.^2)));
+
+        %4) vaccines: DOSE 2:euclidean distance of cumulative numbers 
+       % vx2=daily_c(times(2)+1,5);
+        vx2=daily_c(t,5);
+        vm2=movmean(vx2,7);
+        %take moving average:
+        %vm2=movmean(vaccines(t,2),7);
+    
+        v2=abs(vm2-vaccines(t,2));
+        dists(4)=sqrt(sum((v2.^2)));
+
+        %aboslute difference on day 163 vaccines:
+        %dists(5:6)=abs(inci(163,4:5)-vaccines(163,:));
+        dists(5:6)=abs(inci(times(3)-1,4:5)-vaccine_cums);
+
+        %Re-introduction specific values:
+        ox=daily_c(170:end,1);%generated 
+        dists(7)=abs(sum(ox)-sum(cases(170:end)));
+        
+        
+
+end
